430d818f7ad674797e85e19b3d96c7111e555995
Vitor Calegari <vitorprca@gmail.com>
2023-04-28 11:50:24 -0300

    Completa implementação do exercício 1

--- 1.srp.py
+++ 1.srp.py
@@ -16,3 +16,34 @@ class Animal:
     # salva no DB
     def save(self, animal: Animal):
         pass
+ #-----------------------------------------------------
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def get_name(self) -> str:
+        pass
+
+    # salva no DB
+    def save(self, animal: Animal):
+        pass
+
+# Nova versão: ---------------------------------------------------
+
+class AnimalDB:
+
+    def save(self, animal: Animal):
+        #Salva animal no DB
+        return
+
+class Animal:
+    def __init__(self, name: str):
+        self.__name = name
+        self.__banco_de_dados = AnimalDB()
+    
+    def get_name(self) -> str:
+        return self.__name
+
+    def save(self):
+        self.__banco_de_dados.save(animal = self)
+        return
