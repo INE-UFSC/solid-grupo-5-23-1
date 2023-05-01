b3d39b5ef10dcdf2098fba48db2938ce4cb39f69
Vitor Calegari <vitorprca@gmail.com>
2023-05-01 18:44:58 -0300

    Completa exercicio 5 sobre Dependency Inversion Principle

--- 5.dip.py
+++ 5.dip.py
@@ -4,11 +4,21 @@ Dependency Inversion Principle
 Dependências devem ser feitas sobre abstrações, não sobre implementações concretas 
 
 """
+from abc import ABC, abstractmethod
 
+
+class IStatsReporter(ABC):
+
+    def __init__(self, char: Player):
+        self.__char = char
+
+    @abstractmethod
+    def report(self):
+        pass
 
 class Player:
-    def __init__(self, name):
-        self.stats = StatsReporter(self)
+    def __init__(self, name, stats: IStatsReporter):
+        self.__stats = stats
         self.__name = name
         self.__hp = 100
         self.__speed = 1
@@ -18,11 +28,18 @@ class Player:
 
     def name(self):
         return self.__name
+    
+    def speed(self):
+        return self.__speed
+    
+    def report(self):
+        return self.__stats.report()
 
-class StatsReporter:
+class StatsReporter(IStatsReporter):
     def __init__(self, char: Player):
-        self.char = char
+        super().__init__(char)
 
     def report(self):
-        print(f'Name:{self.char.name()}')
-        print(f'HP:{self.char.hp()}')
+        print(f'Name:{super().char.name()}')
+        print(f'HP:{super().char.hp()}')
+        print(f'Speed:{super().char.speed()}')
